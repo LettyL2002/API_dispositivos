1a0b26dff784b4e0f382ba126aee594d6ec07869
Yuzu <Yuzu_0204@outlook.com>
2024-12-15 03:11:05 -0400

    :bug: Fix CORS to autorize access from the FrontEnd App

--- api/index.js
+++ api/index.js
@@ -3,6 +3,14 @@ import autorRoute from "../routes/autorRoute.js";
 import libroRoute from "../routes/libroRoute.js";
 
 const app = express();
+app.use(cors({
+  origin: [
+    'http://localhost:5173', 
+    'https://front-end-dispositivos.vercel.app'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(json());
 
 app.use("/api/autores", autorRoute);
