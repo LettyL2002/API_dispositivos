bd3f2db40f0d04a55c8a4c92d71770e4c5116612
Yuzu <Yuzu_0204@outlook.com>
2024-12-15 03:21:08 -0400

    feat(CORS): :sparkles: Add CORS middleware for enhanced API access control

--- api/index.js
+++ api/index.js
@@ -1,20 +1,22 @@
 import express, { json } from "express";
 import autorRoute from "../routes/autorRoute.js";
 import libroRoute from "../routes/libroRoute.js";
+import cors from "cors";
 
 const app = express();
-app.use(cors({
-  origin: [
-    'http://localhost:5173', 
-    'https://front-end-dispositivos.vercel.app'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://front-end-dispositivos.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(json());
 
 app.use("/api/autores", autorRoute);
 app.use("/api/libros", libroRoute);
-
 
 export default app;
